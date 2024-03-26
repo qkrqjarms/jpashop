0ce297a5449ed96acfd2d6b40718df8562225a5f
박범근 <qjarms568@naver.com>
2024-03-26 21:23:55 +0900

    멤버 완성.

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String creatForm(Model model){
+        // 값 받아오기.
+        model.addAttribute("memberForm", new MemberForm());
+
+        // 화면 호출.
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String creatMember(@Valid MemberForm form, BindingResult result){
+
+        if ((result.hasErrors())) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/members")
+    public String memberForm(Model model){
+        model.addAttribute("members", memberService.findMembers());
+
+        return "/members/memberList";
+    }
+}
